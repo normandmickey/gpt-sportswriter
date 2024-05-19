ff1c6ad24e0ef051fbc0d35d4704970000a997cc
normandmickey <normandmickey@gmail.com>
2024-05-19 08:03:48 -0400

    changed llm to gpt4

--- gpt_researcher/master/prompts.py
+++ gpt_researcher/master/prompts.py
@@ -46,10 +46,10 @@ def generate_resource_report_prompt(question, context, report_format="MLA", tota
 
     return f'Information: """{context}"""\n\n' \
            f'Using the above information, answer the following' \
-           f' query or task: Write a recap of the game between "{question}" in a detailed report --' \
+           f' query or task: Write a detailed recap of the game between "{question}" in a detailed report --' \
            " The report should focus on the answer to the query, should be well structured, informative, humorous and sarcastic" \
            f" in depth and comprehensive, with facts and numbers if available and a minimum of {total_words} words.\n" \
-           "You should strive to write the report as long as you can using all relevant and necessary information provided including player and team statistics and odds.\n" \
+           "You should strive to write the report as long as you can using all relevant and necessary information provided including player and team statistics and odds and score.\n" \
            "You must write the report with markdown syntax.\n " \
            f"Use an unbiased and journalistic tone but be witty and sarcastic. \n" \
            "You MUST determine your own concrete and valid opinion based on the given information. Do NOT deter to general and meaningless conclusions.\n" \
